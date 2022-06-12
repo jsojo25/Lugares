bc1a76380f1b1edd1315e22c6e3cea4d82157872
jsojo25 <87626528+jsojo25@users.noreply.github.com>
2022-06-12 15:35:31 -0600

    semana 3 avance

---
+++ app/src/main/java/com/example/lugares/data/LugarDataBase.kt
@@ -0,0 +1,35 @@
+package com.example.lugares.data
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.lugares.adapter.LugarDao
+import com.example.lugares.model.Lugar
+
+@Database(entities = [Lugar::class], version = 1, exportSchema = false)
+abstract class LugarDataBase:RoomDatabase()
+{
+    abstract fun lugarDao(): LugarDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: LugarDataBase? = null
+
+
+        fun getDataBase(context: android.content.Context): LugarDataBase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    LugarDataBase::class.java,
+                    "lugar_database"
+                ).build()
+                INSTANCE= instance
+                return instance
+            }
+        }
+    }
+}
